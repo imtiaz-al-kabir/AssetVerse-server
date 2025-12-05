2e187a98f543ef6dcbea0168482e12092db9eb53
Imtiaz AL Kabir <imtiazalkabir@gmail.com>
2025-12-06 01:47:04 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import express from "express";
 import mongoose from "mongoose";
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.json());
