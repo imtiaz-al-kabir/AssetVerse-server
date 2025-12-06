cd71bf8e493aff229c309f85fa3bbd46ad7adbe1
Imtiaz AL Kabir <imtiazalkabir@gmail.com>
2025-12-06 17:02:41 +0600

    update api created

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -21,7 +21,7 @@ export const protect = async (req, res, next) => {
 };
 
 export const admin = (req, res, next) => {
-  if (req.user && req.user.role === "HR") {
+  if (req.user && req.user.role === "hr") {
     next();
   } else {
     res.status(401).json({ message: "Not authorized as an admin" });
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,12 +4,15 @@ import {
   loginUser,
   logoutUser,
   registerUser,
+  updateUserProfile,
 } from "../controllers/authController.js";
+import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 router.post("/register", registerUser);
 router.post("/login", loginUser);
-router.post('/logout', logoutUser);
+router.post("/logout", logoutUser);
+router.put("/profile", protect, updateUserProfile);
 router.get("/me", getCurrentUser);
 
 export default router;
