b2704b260de995f531ceee731aaccb1d6e42f76c
Imtiaz AL Kabir <imtiazalkabir@gmail.com>
2025-12-06 15:36:10 +0600

    middle ware created

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,29 @@
+import jwt from "jsonwebtoken";
+
+import User from "../models/user";
+export const protect = async (req, res, next) => {
+  let token;
+
+  token = req.cookies.jwt;
+
+  if (token) {
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = await User.findById(decoded.id).select("-password");
+      next();
+    } catch (error) {
+      console.error(error);
+      res.status(401).json({ message: "Not authorized, token failed" });
+    }
+  } else {
+    res.status(401).json({ message: "Not authorized, no token" });
+  }
+};
+
+export const admin = (req, res, next) => {
+  if (req.user && req.user.role === "HR") {
+    next();
+  } else {
+    res.status(401).json({ message: "Not authorized as an admin" });
+  }
+};
